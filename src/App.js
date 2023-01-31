c958068fd2f9dbe3f99163b6e1962d6a5bbff367
loretortuiz@hotmail.com <loretoruiz@hotmail.com>
2023-01-31 16:28:53 -0400

    testhola

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.modificado desde rama Robinson modi
+          Edit <code>src/App.js</code> Hola Mundo Cruel
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org" 
           target="_blank"
           rel="noopener noreferrer"
         >
