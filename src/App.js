4c72eaaf78869021009d581af0c34e5b615b29ea
loretortuiz@hotmail.com <loretoruiz@hotmail.com>
2023-01-31 13:43:35 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.Robinson
+          Edit <code>src/App.js</code> and save to reload.modificado desde rama Robinson
         </p>
         <a
           className="App-link"
